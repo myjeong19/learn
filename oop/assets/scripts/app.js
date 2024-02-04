263e582aaf192f90acc0e338dda9e580ecabb3fb
myjeong19@naver.com <myjeong19@naver.com>
2024-02-05 00:41:17 +0900

    20240205

--- oop/assets/scripts/app.js
+++ oop/assets/scripts/app.js
@@ -1,175 +1,182 @@
-// 사진 판매 사이트
-
-//* Photo 객체를 만드는 클래스
-class Photo {
-  constructor(title, imageURL, price, description) {
+//* 객체 Product를 생성함
+class Product {
+  constructor(title, image, desc, price) {
     this.title = title;
-    this.imageURL = imageURL;
+    this.imageUrl = image;
+    this.description = desc;
     this.price = price;
-    this.description = description;
   }
 }
 
-//* ElementAttribute 태그에 속성 부여, 클래스
+//* 생성한 DOM요소에 부여할 속성 값
 class ElementAttribute {
-  constructor(attributeName, attributeValue) {
-    this.name = attributeName;
-    this.value = attributeValue;
+  constructor(attrName, attrValue) {
+    this.name = attrName;
+    this.value = attrValue;
   }
 }
 
+//* DOM요소를 생성할 클래스
+
+//* rootElement로, 첫번째 인자 값 tag를 할당해, DOM요소 생성
+//* shouldRender가 true인 경우, render() 호출
+//* render(){} 명시적 존재
+//* createRootElement 태그, 클래스 명, 속성 값
+//* hookId에, rootElement를 append()
 class Component {
+  // shouldRender 자동 호출 여부 기본 값  true
   constructor(renderHookId, shouldRender = true) {
-    this.hookId = renderHookId;
+    this.renderHookId = renderHookId;
     if (shouldRender) {
       this.render();
     }
   }
 
-  //* 명시적 render
   render() {}
 
-  createRootElement(tag, cssClass, attributes) {
+  createRootElement(tag, cssClasses, attributes) {
     const rootElement = document.createElement(tag);
-    if (cssClass) {
-      //* cssClass가 true인 경우, cssClass 할당
-      rootElement.className = cssClass;
+    if (cssClasses) {
+      rootElement.className = cssClasses;
     }
     if (attributes && attributes.length > 0) {
-      //* attributes가 true이고, 길이가 0 보다 큰 경우
-      for (const attribute of attributes) {
-        //* attributes의 길이만큼 반복하며, setAttribute으로 속성 할당
-        rootElement.setAttribute(attribute.name, attribute.value);
+      for (const attr of attributes) {
+        rootElement.setAttribute(attr.name, attr.value);
       }
     }
-    //* this.hookId는 renderHookId
-    document.getElementById(this.hookId).append(rootElement);
+    document.getElementById(this.renderHookId).append(rootElement);
     return rootElement;
   }
 }
 
-//* Component를 상속 받은 ShoppingCart
+//* 쇼핑 카트
+//* set cartItems 설정 값이 할당 될 때 호출
+//* get totalAmount 값을 읽고 사용할 때 호출
+//* super()를 통해 부모의 constructor를 호출함
 class ShoppingCart extends Component {
-  cartItem = [];
+  items = [];
 
-  //* 장바구니 총액 DOM 업데이트
-  set cartItems(updateCartItem) {
-    this.cartItem = updateCartItem;
-    //* updateCartItem 카트 상품 목록 업데이트한 배열 값
-    this.totalOutput.innerHTML = `<h2>Total: ${this.totalAmount.toLocaleString(
-      'ko-KR'
-    )}원</h2>`;
+  set cartItems(updatedItems) {
+    this.items = updatedItems;
+    this.totalOutput.innerHTML = `<h2>Total: \$${this.totalAmount.toFixed(
+      2
+    )}</h2>`;
   }
 
-  //* 장바구니 총액
   get totalAmount() {
-    const sum = this.cartItem.reduce(
-      (prevValue, currentItem) => prevValue + currentItem.price,
+    const sum = this.items.reduce(
+      (prevValue, curItem) => prevValue + curItem.price,
       0
     );
     return sum;
   }
 
   constructor(renderHookId) {
-    //* 부모 생성자 호출
     super(renderHookId, false);
-    this.orderPhoto = () => {
-      console.log('Ordering...');
-      console.log(this.cartItem);
-    };
+    this.orderProducts = () => console.log(this.items);
     this.render();
   }
 
-  //* 카트에 상품추가
-  handleAddPhoto(photo) {
-    const updatedCartItems = [...this.cartItem];
-    updatedCartItems.push(photo);
-    this.cartItems = updatedCartItems;
+  //* addProduct의 인자 product는 ProductItem의 product임
+  //* 넘겨 받은 값을 updatedItem 배열에 push함.
+  addProduct(product) {
+    const updatedItems = [...this.items];
+    updatedItems.push(product);
+    this.cartItems = updatedItems;
+    //* this.cartItem은 ShoppingCart의 cartItem을 의미함.
   }
 
   render() {
-    //* 부모 Component의 createRootElement
-    const elementCart = this.createRootElement('section', 'cart');
-    elementCart.innerHTML = `
-    <h2>Total: ${0}원</h2>
-    <button>주문하기</button>
+    //* 부모의 createRootElement에, settion과, cart라는 값을 넘겨줌
+    const cartEl = this.createRootElement('section', 'cart');
+    cartEl.innerHTML = `
+      <h2>Total: \$${0}</h2>
+      <button>Order Now!</button>
     `;
-    const orderButton = elementCart.querySelector('button');
-    orderButton.addEventListener('click', this.orderPhoto);
-    this.totalOutput = elementCart.querySelector('h2');
+    const orderButton = cartEl.querySelector('button');
+    // orderButton.addEventListener('click', () => this.orderProducts());
+    orderButton.addEventListener('click', this.orderProducts);
+    this.totalOutput = cartEl.querySelector('h2');
   }
 }
 
-class PhotoItem extends Component {
-  constructor(photo, renderHookId) {
+class ProductItem extends Component {
+  constructor(product, renderHookId) {
     super(renderHookId, false);
-    this.photo = photo;
+    //* 부모인 Compoent의 constructor를 호출.
+    //* 부모인 renderHookId를 참조
+    this.product = product;
     this.render();
   }
 
-  handleAddToCart() {
-    App.addPhotoToCart(this.photo);
+  addToCart() {
+    //* ProductItem의 product를 addProductToCart의 인자로 넘겨줌
+    App.addProductToCart(this.product);
   }
 
   render() {
-    const elementPhotoItem = this.createRootElement('li', 'photo-item');
-    elementPhotoItem.innerHTML = `
-    <div>
-      <img src='${this.photo.imageURL}' alt='${this.photo.title}'>
-      <div>
-          <h2>${this.photo.title}</h2>
-          <h3>${this.photo.price.toLocaleString('ko-KR')}원</h3>
-          <p>${this.photo.description}</p>
-          <button>카트 담기</button>
-      </div>
-    </div>
-    `;
-
-    const buttonAddCart = elementPhotoItem.querySelector('button');
-    buttonAddCart.addEventListener('click', this.handleAddToCart.bind(this));
-    //* this를 bind해, handleAddToCart의 this가 class의 this를 가르키게함
+    //* 부모 Component의 createRootElement 호출해, li에 product-item 클래스를 할당함
+    const prodEl = this.createRootElement('li', 'product-item');
+    prodEl.innerHTML = `
+        <div>
+          <img src="${this.product.imageUrl}" alt="${this.product.title}" >
+          <div class="product-item__content">
+            <h2>${this.product.title}</h2>
+            <h3>\$${this.product.price}</h3>
+            <p>${this.product.description}</p>
+            <button>Add to Cart</button>
+          </div>
+        </div>
+      `;
+    const addCartButton = prodEl.querySelector('button');
+    addCartButton.addEventListener('click', this.addToCart.bind(this));
   }
 }
 
-class PhotoList extends Component {
-  photos = [];
+class ProductList extends Component {
+  #products = [];
 
+  //* Component의 renderHookId에, ProductList()의 인자 값을 전달한다.
   constructor(renderHookId) {
-    super(renderHookId);
-    this.fetchPhotos();
+    super(renderHookId, false);
+
+    //* this는 ProductList를 가르킴
+    //* 즉 constructor가 호출되면 ProductList의 render()와, fetchProducts()가 호출됨.
+    this.render();
+    this.fetchProducts();
   }
 
-  fetchPhotos() {
-    this.photos = [
-      new Photo(
-        'Photo 1',
-        'https://images.unsplash.com/photo-1682687220742-aba13b6e50ba?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        20000,
-        'This is Photo 1'
+  fetchProducts() {
+    this.#products = [
+      new Product(
+        'A Pillow',
+        'https://www.maxpixel.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg',
+        'A soft pillow!',
+        19.99
       ),
-
-      new Photo(
-        'Photo 2',
-        'https://images.unsplash.com/photo-1697462247864-338e7eba8c4c?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        15000,
-        'This is Photo 2'
+      new Product(
+        'A Carpet',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg',
+        'A carpet which you might like - or not.',
+        89.99
       ),
     ];
-    this.renderPhotos();
+    this.renderProducts();
   }
 
-  renderPhotos() {
-    for (const photo of this.photos) {
-      new PhotoItem(photo, 'photo-list');
+  renderProducts() {
+    for (const prod of this.#products) {
+      new ProductItem(prod, 'prod-list');
     }
   }
 
   render() {
-    this.createRootElement('ul', 'photo-list', [
-      new ElementAttribute('id', 'photo-list'),
+    //* 부모인 Compoent에서 받아온 createRootElement를 가르킴
+    this.createRootElement('ul', 'product-list', [
+      new ElementAttribute('id', 'prod-list'),
     ]);
-    if (this.photos && this.photos.length > 0) {
-      this.renderPhotos();
+    if (this.#products && this.#products.length > 0) {
+      this.renderProducts();
     }
   }
 }
@@ -180,22 +187,23 @@ class Shop {
   }
 
   render() {
-    //* 1-1 this.createCart는 Shop의 createCart를 의미함.
-    this.createCart = new ShoppingCart('app');
-    new PhotoList('app');
+    this.cart = new ShoppingCart('app');
+    new ProductList('app');
   }
 }
 
+//* this.cart는 App의 static이며, this.cart는 shop.cart임
+//* shop의 cart에, 넘겨주기 위해, App의 addProductToCart에, product를 받아, addProduct를 호출해 넘겨줌
 class App {
-  static createCart;
+  static cart;
 
   static init() {
     const shop = new Shop();
-    this.createCart = shop.createCart;
+    this.cart = shop.cart;
   }
 
-  static addPhotoToCart(photo) {
-    this.createCart.handleAddPhoto(photo);
+  static addProductToCart(product) {
+    this.cart.addProduct(product);
   }
 }
 
