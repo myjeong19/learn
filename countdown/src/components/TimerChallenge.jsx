abb192d45d10c1f58e0f9ccd0504ef91f9a3e28a
myjeong19@naver.com <myjeong19@naver.com>
2024-02-20 01:13:41 +0900

    useRef()

---
+++ countdown/src/components/TimerChallenge.jsx
@@ -0,0 +1,34 @@
+import { useState, useRef } from 'react';
+
+let timer;
+
+export const TimerChallenge = ({ title, targetTime }) => {
+  const timer = useRef();
+
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  const handleStart = () => {
+    timer.current = setTimeout(() => setTimerExpired(true), targetTime * 1000);
+
+    setTimerStarted(true);
+  };
+
+  const handleStop = () => clearTimeout(timer.current);
+
+  return (
+    <section className='challenge'>
+      <h2>{title}</h2>
+      {timerExpired && <p>You lost!</p>}
+      <p className='challenge-time'>
+        {targetTime} second{targetTime > 1 ? 's' : null}
+      </p>
+      <button onClick={timerStarted ? handleStop : handleStart}>
+        {timerStarted ? 'Stop' : 'Start'} Challenge
+      </button>
+      <p className={timerStarted ? 'active' : null}>
+        {timerStarted ? 'Time is running...' : 'Timer inactive'}
+      </p>
+    </section>
+  );
+};
