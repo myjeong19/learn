a4b189ac9498878b5841066746455788dad1dd96
정민영 <myjeong19@naver.com>
2024-03-25 15:01:28 +0900

    Create. base-lazy-days

--- base-blog-em/src/App.jsx
+++ base-blog-em/src/App.jsx
@@ -1,8 +1,8 @@
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import { Posts } from './Posts';
-import './App.css';
+import { Posts } from "./Posts";
+import "./App.css";
 
 const queryClient = new QueryClient();
 
@@ -10,7 +10,7 @@ function App() {
   return (
     // provide React Query client to App
     <QueryClientProvider client={queryClient}>
-      <div className='App'>
+      <div className="App">
         <h1>Blog &apos;em Ipsum</h1>
         <Posts />
       </div>
