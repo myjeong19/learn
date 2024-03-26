feb5c19e9645efc6264879962414d2800e0310ec
정민영 <myjeong19@naver.com>
2024-03-27 00:53:37 +0900

    타입 추론

--- basics-ts/benefits/01basic.ts
+++ basics-ts/benefits/01basic.ts
@@ -1,12 +1,12 @@
-function jsAdd(n1, n2) {
-  return n1 + n2;
-}
+// function jsAdd(n1, n2) {
+//   return n1 + n2;
+// }
 
-const jsNumber1 = "5";
-const jsNumber2 = 2.8;
+// const jsNumber1 = "5";
+// const jsNumber2 = 2.8;
 
-const jsResult = tsAdd(jsNumber1, jsNumber2);
-console.log(jsResult); // 52.8
+// const jsResult = jsAdd(jsNumber1, jsNumber2);
+// console.log(jsResult); // 52.8
 // *  Argument of type 'string' is not assignable to parameter of type 'number'.
 
 function tsAdd(n1: number, n2: number) {
