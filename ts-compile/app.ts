c74da1548b52c7a5c4dd115988ab1eb48bda2223
정민영 <myjeong19@naver.com>
2024-04-10 17:21:10 +0900

    FEAT. Compiler Options lib

--- ts-compile/app.ts
+++ ts-compile/app.ts
@@ -1,4 +1,10 @@
-let count: number;
+// let count: number;
 
-count = 5;
-console.log(count);
+// count = 5;
+// console.log(count);
+
+const button = document.querySelector('button')!;
+
+button.addEventListener('click', () => {
+  console.log('Clicked!');
+});
