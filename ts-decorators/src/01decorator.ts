85e6f475608b9df228ee1ed2d5af4e25bfe4e730
정민영 <myjeong19@naver.com>
2024-04-17 23:44:00 +0900

    FEAT. Decorator

---
+++ ts-decorators/src/01decorator.ts
@@ -0,0 +1,16 @@
+function Logger(constructor: Function) {
+  console.log('Logging...');
+  console.log(constructor);
+}
+@Logger
+class Person {
+  name = 'Max';
+
+  constructor() {
+    console.log('Creating person object...');
+  }
+}
+
+const person = new Person();
+
+console.log(person);
