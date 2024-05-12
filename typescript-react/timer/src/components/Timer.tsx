b8b21840c34fbc762200db1073a623f6accbe955
정민영 <myjeong19@naver.com>
2024-05-12 21:38:06 +0900

    FEAT. Timer

--- typescript-react/timer/src/components/Timer.tsx
+++ typescript-react/timer/src/components/Timer.tsx
@@ -1,11 +1,48 @@
+import { useState, useEffect, useRef } from 'react';
+import { Timer as TimerProps, useTimersContext } from '../store/timers-context.tsx';
 import Container from './UI/Container.tsx';
-import { Timer as TimerProps } from '../store/timers-context.tsx';
 
 export default function Timer({ name, duration }: TimerProps) {
+  const interval = useRef<number | null>(null);
+  // 변수와 다르게 재생성 되지 않는다.
+
+  const [remainingTime, setRemainingTime] = useState(duration * 1000);
+  const { isRunning } = useTimersContext();
+
+  if (remainingTime <= 0 && interval.current) {
+    clearInterval(interval.current);
+  }
+
+  useEffect(() => {
+    let timer: number;
+
+    if (isRunning) {
+      timer = setInterval(() => {
+        setRemainingTime(prevTime => {
+          if (prevTime <= 0) {
+            return prevTime;
+          }
+          return prevTime - 50;
+        });
+      }, 50);
+      interval.current = timer;
+    } else if (interval.current) {
+    }
+
+    return () => clearInterval(timer);
+    // function or nothing
+    // cleanup function call by React
+  }, [isRunning]);
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+
   return (
     <Container as="article">
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p>
+        <progress max={duration * 1000} value={remainingState} />
+        <p>{formattedRemainingTime}</p>
+      </p>
     </Container>
   );
 }
