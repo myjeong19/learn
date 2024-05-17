d9c18a0bb0d607e8f78e73d529d6bd3b5f5b472c
정민영 <myjeong19@naver.com>
2024-05-17 19:07:25 +0900

    FEAT. Add Link

--- javascript/interview/working-with-dom/HighlistAllWordOver 8CharsWithYello/Highlight-All-Words-Over-8-Chars-With-Yellow/main.js
+++ javascript/interview/working-with-dom/HighlistAllWordOver 8CharsWithYello/Highlight-All-Words-Over-8-Chars-With-Yellow/main.js
@@ -5,3 +5,9 @@ const paragraph = document.querySelector('p');
 paragraph.innerHTML = paragraph.innerHTML
   .split(' ')
   .map(word => (word.length > 8 ? `<span style="background-color: yellow">${word}</span>` : word));
+
+// Add a link back to the source of the text after the paragraph tag.
+const link = document.createElement('a');
+link.href = 'https://foremipsum.com/';
+link.innerText = 'Text generated from Lorem Ipsum';
+document.body.appendChild(link);
