c70fb7ebadba53ce38ac0598c071ae5e73376b71
정민영 <myjeong19@naver.com>
2024-05-18 12:46:18 +0900

    REFACTOR. counter

---
+++ javascript/review/src/counter/handler.js
@@ -0,0 +1,15 @@
+import { elementTextarea, elementTextCounter } from './element.js';
+
+export default function handlerInput() {
+  // determine maximum number of characters
+  const maxNrChars = 150;
+
+  // determine number of characters currently typed
+  const nrCharsTyped = elementTextarea.value.length;
+
+  // calculate number of characters left (maximum minus currently typed)
+  const charsLeft = maxNrChars - nrCharsTyped;
+
+  // show number of characters left
+  elementTextCounter.textContent = charsLeft;
+}
--- javascript/review/src/counter/index.js
+++ javascript/review/src/counter/index.js
@@ -1,17 +1,4 @@
-import { elementTextarea, elementTextCounter } from './element.js';
-
-export default function handlerInput() {
-  // determine maximum number of characters
-  const maxNrChars = 150;
-
-  // determine number of characters currently typed
-  const nrCharsTyped = elementTextarea.value.length;
-
-  // calculate number of characters left (maximum minus currently typed)
-  const charsLeft = maxNrChars - nrCharsTyped;
-
-  // show number of characters left
-  elementTextCounter.textContent = charsLeft;
-}
+import { elementTextarea } from './element.js';
+import handlerInput from './handler.js';
 
 export { elementTextarea, handlerInput };
