3ce905e3f2f06d17122dcd0294a1938deded5177
정민영 <myjeong19@naver.com>
2024-05-20 00:27:46 +0900

    FEAT. Spread Operator

---
+++ javascript/advanced/spreadOperator.js
@@ -0,0 +1,7 @@
+// spread operator
+
+const numbersFirst = [1, 2, 3];
+const numbersSecond = [4, 5, 6];
+
+const combinedNumbers = [...numbersFirst, ...numbersSecond];
+console.log(combinedNumbers); // [1, 2, 3, 4, 5, 6]
