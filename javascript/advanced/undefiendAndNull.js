a94ad6df29597b998614c5c9f59099b7230c814e
정민영 <myjeong19@naver.com>
2024-05-25 23:27:35 +0900

    FEAT. undefined & null

---
+++ javascript/advanced/undefiendAndNull.js
@@ -0,0 +1,16 @@
+// undefined and null
+
+// undefined
+// initial value
+let number;
+console.log(number); // undefined
+
+const data = {
+  cities: ['New York', 'Los Angeles', 'Chicago'],
+  month: 'July',
+};
+
+// null
+// explicitly
+let city = null;
+console.log(city); // null
