0d02f76b317ce8e689efa4df09bad1be67135aab
정민영 <myjeong19@naver.com>
2024-05-27 01:03:45 +0900

    REFACTOR. Rewrite Mapping Data in Async Await

--- javascript/interview/asynchronous/MapDataInPromises/app.js
+++ javascript/interview/asynchronous/MapDataInPromises/app.js
@@ -63,12 +63,30 @@ const getUserStatuses = () => {
 //   console.log('mappedUsers', mapppedUsers);
 // });
 
-const mapUsers = (users, userStatuses) =>
-  users.map(user => {
-    const isActive = userStatuses.find(userStatus => userStatus.id === user.id).isActive;
-    return { ...user, isActive };
-  });
+// const mapUsers = (users, userStatuses) =>
+//   users.map(user => {
+//     const isActive = userStatuses.find(userStatus => userStatus.id === user.id).isActive;
+//     return { ...user, isActive };
+//   });
 
-Promise.all([getUsers(), getUserStatuses()])
-  .then(([users, userStatuses]) => mapUsers(users, userStatuses))
-  .then(mappedUsers => console.log('mappedUsers', mappedUsers));
+// Promise.all([getUsers(), getUserStatuses()])
+//   .then(([users, userStatuses]) => mapUsers(users, userStatuses))
+//   .then(mappedUsers => console.log('mappedUsers', mappedUsers));
+
+const getMappedUsers = async () => {
+  try {
+    const users = await getUsers();
+    const userStatuses = await getUserStatuses();
+
+    const mappedUsers = users.map(user => {
+      const isActive = userStatuses.find(userStatus => userStatus.id === user.id).isActive;
+      return { ...user, isActive };
+    });
+
+    console.log('mappedUsers', mappedUsers);
+  } catch (error) {
+    console.log('error', error);
+  }
+};
+
+getMappedUsers();
