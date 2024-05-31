958cd25b7cfd28d107738e7f0ae6e74d9dde53a4
정민영 <myjeong19@naver.com>
2024-05-31 15:07:20 +0900

    FEAT. What are class components, props and state

---
+++ react/interview/basicKnowledge/react/src/App.jsx
@@ -0,0 +1,16 @@
+import WhatAreFunctionalComponentsAndProps from './WhatAreFunctionalComponentsAndProps';
+import WhatAreClassComponentsPropsandState from './WhatAreClassComponentsPropsandState';
+
+const App = () => {
+  return (
+    <div>
+      <WhatAreFunctionalComponentsAndProps />
+
+      <hr />
+
+      <WhatAreClassComponentsPropsandState />
+    </div>
+  );
+};
+
+export default App;
