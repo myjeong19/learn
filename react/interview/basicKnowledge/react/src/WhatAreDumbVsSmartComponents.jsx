03278f0c28cc530d9781495b90110f429332fda5
정민영 <myjeong19@naver.com>
2024-05-31 16:05:31 +0900

    FEAT. What are Dumb vs Smart Components

---
+++ react/interview/basicKnowledge/react/src/WhatAreDumbVsSmartComponents.jsx
@@ -0,0 +1,8 @@
+// Question
+// What are dumb vs smart components?
+// What are presentational vs container components?
+
+// dumb와, presentational은 동일 하며, smart와 container는 동일하다.
+// 어떤 상태 논리도 없고, 단순히 일부 데이터를 렌더링하는 경우 presentational component라고 한다.
+
+// 내부 상태가 있는 경우 container component라고 한다.
