ec4310f336c7ced5a5ca2d752484f68913f1e891
정민영 <myjeong19@naver.com>
2024-05-31 16:26:33 +0900

    FEAT. What is conditional rendering in React

--- react/interview/basicKnowledge/react/src/App.jsx
+++ react/interview/basicKnowledge/react/src/App.jsx
@@ -2,23 +2,20 @@ import WhatAreFunctionalComponentsAndProps from './WhatAreFunctionalComponentsAn
 import WhatAreClassComponentsPropsandState from './WhatAreClassComponentsPropsandState';
 import WhatIsAKeyIndexMap from './WhatIsAKeyIndexMap';
 import WhatIsReactFragment from './WhatIsReactFragment';
+import WhatIsConditionalRenderingInReact from './WhatIsConditionalRenderingInReact';
 
 const App = () => {
   return (
     <div>
       <WhatAreFunctionalComponentsAndProps />
-
       <hr />
-
       <WhatAreClassComponentsPropsandState />
-
       <hr />
-
       <WhatIsAKeyIndexMap />
-
       <hr />
-
       <WhatIsReactFragment />
+      <hr />
+      <WhatIsConditionalRenderingInReact />
     </div>
   );
 };
