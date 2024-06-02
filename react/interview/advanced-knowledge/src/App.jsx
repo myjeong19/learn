9650d4438b38704317c76326143952074a1b2992
정민영 <myjeong19@naver.com>
2024-06-02 22:42:54 +0900

    FEAT. useCallback

--- react/interview/advanced-knowledge/src/App.jsx
+++ react/interview/advanced-knowledge/src/App.jsx
@@ -1,28 +1,25 @@
 import './App.css';
-import WhatIsUseEffect from './components/WhatIsUseEffect';
-import WhatIsUseMemo from './components/WhatIsUseMemo';
-import { WhatIsUseReducer } from './components/WhatIsUseReducer';
-import { WhatIsUseRef } from './components/WhatIsUseRef';
-import WhatIsUseState from './components/WhatIsUseState';
-import { Container } from './components/WhatisUseContext/Container';
-import { ThemeContextProvider } from './components/WhatisUseContext/ThemeContextProvider';
+import WhatIsUseCallback from './components/WhatIsUseCallback';
+// import WhatIsUseEffect from './components/WhatIsUseEffect';
+// import WhatIsUseMemo from './components/WhatIsUseMemo';
+// import { WhatIsUseReducer } from './components/WhatIsUseReducer';
+// import { WhatIsUseRef } from './components/WhatIsUseRef';
+// import WhatIsUseState from './components/WhatIsUseState';
+// import { Container } from './components/WhatisUseContext/Container';
+// import { ThemeContextProvider } from './components/WhatisUseContext/ThemeContextProvider';
 
 function App() {
   return (
     <>
-      <WhatIsUseState />
-      <hr />
-      <WhatIsUseEffect />
-      <hr />
-      <WhatIsUseReducer />
-      <hr />
-      <ThemeContextProvider>
+      {/* <WhatIsUseState /> */}
+      {/* <WhatIsUseEffect /> */}
+      {/* <WhatIsUseReducer /> */}
+      {/* <ThemeContextProvider>
         <Container />
-      </ThemeContextProvider>
-      <hr />
-      <WhatIsUseRef />
-      <hr />
-      <WhatIsUseMemo />
+      </ThemeContextProvider> */}
+      {/* <WhatIsUseRef /> */}
+      {/* <WhatIsUseMemo /> */}
+      <WhatIsUseCallback />
     </>
   );
 }
---
+++ react/interview/advanced-knowledge/src/components/WhatIsUseCallback.jsx
@@ -0,0 +1,47 @@
+// what do you know about useCallback?
+// useCallback은 함수 호출을 캐시하는 데 사용된다.
+// useCallback 훅을 사용하지 않으면 자식 컴포넌트가 불필요하게 다시 렌더된다.
+
+import { useState, useCallback } from 'react';
+
+const initialUsers = [
+  { id: 1, name: 'Robin' },
+  { id: 2, name: 'Dennis' },
+  { id: 3, name: 'Morgan' },
+];
+
+const List = ({ users, onRemove }) => {
+  return (
+    <ul>
+      {users.map(({ id, name }) => (
+        <li key={id}>
+          {name}
+          <span onClick={() => onRemove(id)}></span>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const WhatIsUseCallback = () => {
+  console.log('render');
+  const [users, setUsers] = useState(initialUsers);
+  const [text, setText] = useState('');
+
+  const handleRemove = useCallback(
+    id => {
+      const updateUsers = users.filter(user => user.id !== id);
+      setUsers(updateUsers);
+    },
+    [users]
+  );
+
+  return (
+    <>
+      <input type="text" value={text} onChange={e => setText(e.target.value)} />
+      <List users={users} onRemove={handleRemove} />
+    </>
+  );
+};
+
+export default WhatIsUseCallback;
