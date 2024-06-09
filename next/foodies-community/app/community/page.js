4fd2b7e2e77d9ab1150a2edd8ff8ec85c5a6b4d2
정민영 <myjeong19@naver.com>
2024-06-09 23:38:07 +0900

    STYLE. Header

---
+++ next/foodies-community/app/community/page.js
@@ -0,0 +1,7 @@
+export default function CommunityPage() {
+  return (
+    <main>
+      <h1>This is Community</h1>
+    </main>
+  );
+}
---
+++ next/foodies-community/app/meals/[mealSlug]/page.js
@@ -0,0 +1,7 @@
+export default function MealDetailsPage() {
+  return (
+    <main>
+      <h1>MealDetailsPage</h1>
+    </main>
+  );
+}
