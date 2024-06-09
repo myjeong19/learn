1f18c8605e46505812a2ca5fbca1b63ac3f76784
정민영 <myjeong19@naver.com>
2024-06-09 19:55:54 +0900

    FEAT. Asyncronous Programming

--- node/custom-module/main.js
+++
@@ -1,8 +0,0 @@
-const logger = require('./logger');
-
-const { config } = require('./logger');
-
-logger('this is a log message from main.js file');
-
-console.log('Log file name:', logger.config.logFileName);
-console.log('Log directory:', config.logDirectory);
