dd495666f6a4eb2b94c7e0ea011d5dfa535ab269
정민영 <myjeong19@naver.com>
2024-06-09 20:48:28 +0900

    FEAT. HTTP headers

--- node/api/index.js
+++ node/api/index.js
@@ -4,7 +4,12 @@ const http = require('http');
 //* 서버가 필요한 이유
 //* 서버는 요청을 처리하고 사용자와 애플리케이션 간의 응답을 제공하는 게이트웨이 역할을 한다.
 const server = http.createServer((req, res) => {
+  //   res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.setHeader('Content-Type', 'text/plain');
+  res.statusCode = 200;
   res.end('Hello, This is a NodeJs HTTP Server');
+  //   res.writeHead(200, { 'Content-Type': 'text/html' });
+  //   res.end("Hello, This is a NodeJs HTTP Server <p style='color: green'>200 ok</p>");
 });
 
 server.listen(3000, () => {
