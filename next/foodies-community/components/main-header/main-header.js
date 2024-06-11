27f0b43c6439daa9ffbe79f8f5b74423d92fd7cb
정민영 <myjeong19@naver.com>
2024-06-11 23:38:39 +0900

    STYLE. Header

---
+++ next/foodies-community/components/main-header/main-header.js
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+import logoImg from '@/assets/logo.png';
+import MainHeaderBackground from './main-haeder-background';
+
+import classes from './css/main-header.module.css';
+
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} alt="A plate with food on it" property />
+          NextLevel Food
+        </Link>
+
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
