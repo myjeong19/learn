baaf88d2d9c1a03ab592a0e9085929197b5f81ff
정민영 <myjeong19@naver.com>
2024-06-18 20:19:00 +0900

    STYLE Meal Details

--- next/foodies-community/app/meals/[mealSlug]/page.js
+++ next/foodies-community/app/meals/[mealSlug]/page.js
@@ -1,7 +1,34 @@
-export default function MealDetailsPage() {
+import Image from 'next/image';
+
+import classes from './css/page.module.css';
+import { getMeal } from '@/lib/meal';
+export default function MealDetailsPage({ params }) {
+  let { title, image, creator_email, creator, summary, instructions } = getMeal(params.mealSlug);
+
+  instructions = instructions.replace(/<p>/g, '').replace(/\n/g, '<br>');
+
   return (
-    <main>
-      <h1>MealDetailsPage</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={image} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${creator_email}`}>{creator}</a>
+          </p>
+          <p className={classes.summary}>{summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: instructions,
+          }}
+        ></p>
+      </main>
+    </>
   );
 }
