3a1c9ae17148fe660e5c4159dbe3515ecb490ffa
정민영 <myjeong19@naver.com>
2024-06-18 21:23:57 +0900

    FEAT> Notfound Meal

--- next/foodies-community/app/meals/[mealSlug]/page.js
+++ next/foodies-community/app/meals/[mealSlug]/page.js
@@ -2,30 +2,35 @@ import Image from 'next/image';
 
 import classes from './css/page.module.css';
 import { getMeal } from '@/lib/meal';
+import { notFound } from 'next/navigation';
 export default function MealDetailsPage({ params }) {
-  let { title, image, creator_email, creator, summary, instructions } = getMeal(params.mealSlug);
+  let meal = getMeal(params.mealSlug);
 
-  instructions = instructions.replace(/<p>/g, '').replace(/\n/g, '<br>');
+  meal.instructions = meal.instructions.replace(/<p>/g, '').replace(/\n/g, '<br>');
+
+  if (!meal) {
+    notFound();
+  }
 
   return (
     <>
       <header className={classes.header}>
         <div className={classes.image}>
-          <Image src={image} fill />
+          <Image src={meal.image} fill />
         </div>
         <div className={classes.headerText}>
-          <h1>{title}</h1>
+          <h1>{meal.title}</h1>
           <p className={classes.creator}>
-            by <a href={`mailto:${creator_email}`}>{creator}</a>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
           </p>
-          <p className={classes.summary}>{summary}</p>
+          <p className={classes.summary}>{meal.summary}</p>
         </div>
       </header>
       <main>
         <p
           className={classes.instructions}
           dangerouslySetInnerHTML={{
-            __html: instructions,
+            __html: meal.instructions,
           }}
         ></p>
       </main>
