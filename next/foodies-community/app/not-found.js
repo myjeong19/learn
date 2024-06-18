b907d185dbc4229832c5d49a0114f6d5e09da90b
정민영 <myjeong19@naver.com>
2024-06-18 20:06:49 +0900

    STYLE. Not Found

---
+++ next/foodies-community/app/not-found.js
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Unfortunately, we could not find the requested page or resource.</h1>
+    </main>
+  );
+}
