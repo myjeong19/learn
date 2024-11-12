905df5885223813d3e78f89abefd437c39afc113
myjeong19 <myjeong19@naver.com>
2024-11-12 21:30:44 +0900

    ♻️ Refactor export

--- svelte/src/lib/components/reactivity/index.ts
+++ svelte/src/lib/components/reactivity/index.ts
@@ -1,3 +1,3 @@
 export { default as State } from './state.svelte';
 export { default as Effect } from './effect.svelte';
-export { default as Counter } from './shared/counter-render.svelte';
+export * from './shared';
