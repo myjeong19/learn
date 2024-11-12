4c10ad61176d7eaefce7065d5174f87df95e92b1
myjeong19 <myjeong19@naver.com>
2024-11-12 21:35:44 +0900

    ✨ Added Routes logic

--- svelte/src/routes/+layout.svelte
+++ svelte/src/routes/+layout.svelte
@@ -7,6 +7,7 @@
 <nav>
 	<a class="w-20 rounded bg-black px-3 py-1 text-white" href="/state">state</a>
 	<a class="w-20 rounded bg-black px-3 py-1 text-white" href="/props">props</a>
+	<a class="w-20 rounded bg-black px-3 py-1 text-white" href="/logic">logic</a>
 </nav>
 
 {@render children()}
