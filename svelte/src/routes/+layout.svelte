197325f1ef3e6f63be21897eea7c5336228ea734
myjeong19 <myjeong19@naver.com>
2024-11-12 21:32:06 +0900

    ✨ Add props link

--- svelte/src/routes/+layout.svelte
+++ svelte/src/routes/+layout.svelte
@@ -6,6 +6,7 @@
 
 <nav>
 	<a class="w-20 rounded bg-black px-3 py-1 text-white" href="/state">state</a>
+	<a class="w-20 rounded bg-black px-3 py-1 text-white" href="/props">props</a>
 </nav>
 
 {@render children()}
