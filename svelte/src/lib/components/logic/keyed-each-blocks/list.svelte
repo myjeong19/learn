dcea7eee588d48b69302b1f0f7cf8c28f26d4ba9
myjeong19 <myjeong19@naver.com>
2024-11-13 02:52:58 +0900

    ♻️ Added comment

--- svelte/src/lib/components/logic/keyed-each-blocks/list.svelte
+++ svelte/src/lib/components/logic/keyed-each-blocks/list.svelte
@@ -7,6 +7,7 @@
 
 <button onclick={() => things.shift()}> Remove first thing </button>
 
+<!-- Svelte는 내부적으로 Map을 사용하므로 어떤 객체든 키로 사용할 수 있습니다. -->
 {#each things as thing (thing.id)}
 	<Thing name={thing.name} />
 {/each}
