6138ed89e98a8ae762e5ac620a447258b7efe703
myjeong19 <myjeong19@naver.com>
2024-12-30 20:16:05 +0900

    🔧 Alias

--- react/docs/tic-tac-toe/vite.config.ts
+++ react/docs/tic-tac-toe/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '~app': path.resolve(__dirname, './src/app'),
+      '~assets': path.resolve(__dirname, './src/assets'),
+      '~entities': path.resolve(__dirname, './src/entities'),
+      '~features': path.resolve(__dirname, './src/features'),
+      '~shared': path.resolve(__dirname, './src/shared'),
+    },
+  },
+});
